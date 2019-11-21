3c97eb0a222e035451a261be9bc93ec402f58f6d
YuraBobeshko <gorobech55@gmail.com>
2019-11-21 23:10:44 +0200

    Updates

---
+++ precache-manifest.3e832ddba075505a56e030793410dc5a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "74834dad684869cdabe466bd3c7cc264",
+    "url": "/react_tabs-with-router/index.html"
+  },
+  {
+    "revision": "d09b16080cdf7b7e658d",
+    "url": "/react_tabs-with-router/static/css/main.36497136.chunk.css"
+  },
+  {
+    "revision": "cfb38fe16a8c3f999c5f",
+    "url": "/react_tabs-with-router/static/js/2.e1e66e08.chunk.js"
+  },
+  {
+    "revision": "d09b16080cdf7b7e658d",
+    "url": "/react_tabs-with-router/static/js/main.3c56a7ad.chunk.js"
+  },
+  {
+    "revision": "da3ca6fceb301548d22d",
+    "url": "/react_tabs-with-router/static/js/runtime~main.5858125e.js"
+  }
+]);
